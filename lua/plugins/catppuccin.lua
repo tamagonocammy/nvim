b31e8101ca7d438811ef57da2ef2022523c1aad8
Camila Valencia <jcvc2000@gmail.com>
2023-03-27 14:55:32 -0500

    initial commit

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,11 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin" },
+
+  -- Configure LazyVim to load catppuccin
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
+}
